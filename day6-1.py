d540180e3c895247754931a96ad9c8f99565f8fb
Paul Wichser <paul@paulwichser.com>
2019-12-07 09:47:40 -0600

    day 6 open file

--- day6-1.py
+++ day6-1.py
@@ -6,12 +6,22 @@ class Orb(object):			#Orbit object that has a name and a single parent
 		#iterate through all parents and increment by 1 for each until None
 		print('count method initiated')
 		cnt = 0
-#		X = self.parent
-#		while X.parent.name != None:
-#			cnt +1
-#			X = X.parent
-#			print(cnt)
+		X = self
+		while X.parent != None:
+			print(X.name)
+			cnt+=1
+			X = X.parent
+			print(cnt)
 		return cnt
+
+#build orbits from file function
+def orbuild(filename):
+	file = open(filename,'r')
+	
+
+#test section
+file = open('day6-1test.txt','r')
+print(file.read())
 
 #take input from command line for data text file
 #read data text file to data
@@ -20,24 +30,25 @@ class Orb(object):			#Orbit object that has a name and a single parent
 #iterate through list and sum Orb.count
 	#temporary Orb
 
-A = Orb()
-A.name = 'A'
-B = Orb()
-B.name = 'B'
-B.parent = A
+#A = Orb()
+#A.name = 'A'
+#B = Orb()
+#B.name = 'B'
+#B.parent = A
 #print(B.name)
 #print(B.parent.name)
 #print(B.parent.parent)
 
-D = Orb()
-D.parent = B
-D.name = 'D'
-C = Orb()
-C.parent = B
-C.name = 'C'
-orbits = [A,B,C,D]
+#D = Orb()
+#D.parent = B
+#D.name = 'D'
+#C = Orb()
+#C.parent = B
+#C.name = 'C'
+#orbits = [A,B,C,D]
 #X = orbits[2]
 #print(X.name)
-C.count()
-orbits[2].count()
-Y = orbits[2].parent
+#print(C.count())
+#print(orbits[2].count())
+#Y = orbits[2].parent
+#print(Y.count())
