d6cd7a054959ba5d0fd11e6fb713f499bd407340
Paul Wichser <paul@paulwichser.com>
2019-12-09 19:22:04 -0600

    fileimp methods started. need more on wireimp

---
+++ fileimp.py
@@ -0,0 +1,17 @@
+def intimp(filename):
+    with open(filename,'r') as file:
+        for line in file:
+            line = line.rstrip('\n')
+            list = line.split(',')
+            # print(len(list))
+        for x in range(len(list)):
+            list[x] = int(list[x])
+    print("Imported int list of length %i" % len(list))
+    return list
+
+def wireimp(filename):
+    with open(filename,'r') as file:
+        for line in file:
+            line = line.rstrip('\n')
+            list = line.split(',')
+            
