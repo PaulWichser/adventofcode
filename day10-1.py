cdf1ddd93bcde596181a5c77709484f1c731d020
Paul Wichser <paul@paulwichser.com>
2019-12-13 10:16:00 -0600

    day10-1 slope start

--- day10-1.py
+++ day10-1.py
@@ -9,6 +9,8 @@ def bestaster(asters):
                 for k in range(len(asters)):
                     for l in range(len(asters[k])):
                         if asters[k][l] == 1:
-                            slopes['%']
+                            slope = (j-l)/(i-k)
+                            slopes
+                            slopes['%d' % slope].append()
 
 asters = fileimp.asterimp('day10-1test1.txt')
